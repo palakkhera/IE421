a5c6a49eaf51471d2deba6619a0962a3ac4f05b4
Zokonkwo <112595837+Zokonkwo@users.noreply.github.com>
2025-04-09 21:20:24 -0500

    Update bram.sv

--- bram.sv
+++ bram.sv
@@ -1,1 +1,12 @@
+/*
+BRAM --> Block Random Access Memory
+  Used to store large amounts of memory in your FPGA
+  Close proximity to the FPGA logic circuits --> high speed access 
+  Good for rapid retrieval 
+  Often used to store Lookup tables
 
+Inputs:
+Outputs:
+Goal:
+  Provide fast and effienct memory storage for data and intructions
+*/
